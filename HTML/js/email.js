196eb40863fb3edd2c0561a89a708427a580af29
martinnicola89 <martinnicola89@gmail.com>
2021-09-29 12:51:17 -0400

    Update site content and CSS

---
+++ HTML/js/email.js
@@ -0,0 +1,25 @@
+function sendMail(params) {
+    var tempParams = {
+        name: document.getElementById('name').value,
+        user_email: document.getElementById('user_email').value,
+        message: document.getElementById('message').value,
+    }
+    emailjs.send('service_jym8ivl','template_t0fyrfa', tempParams)
+    .then(function(res){
+        console.log('success',res.status)
+    })
+}
+
+function clearField(params) {
+    document.getElementById('name').value = ""
+    document.getElementById('user_email').value = ""
+    document.getElementById('message').value = ""
+}
+
+function success() {
+    Swal.fire({
+      icon: "success",
+      title: "Thank you...",
+      text: "We'll brew your reply shortly!",
+    });
+  }
